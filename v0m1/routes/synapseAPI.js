08a63abb65e5d5289b3cf3b718b51f14740678d7
Nathan N <Nitharios@users.noreply.github.com>
2018-09-12 19:11:33 -1000

    Updated Users route

--- v0m1/routes/synapseAPI.js
+++ v0m1/routes/synapseAPI.js
@@ -23,17 +23,137 @@ router.route('/')
     query: ''
   }
 
-  return Users.get(
+  Users.get(
     client,
     options,
-    (err, res) => {
+    (err, usersRes) => {
       if (err) {
         console.log('Users error', err);
-        return;
+        return res.json(err);
       }
 
-      console.log('Users', res)
-      return res;
+      console.log('Users', usersRes)
+      return res.json(usersRes);
     }
+  )
+})
+.post((req, res) => {
+  const payload = {
+    logins: [
+      {
+        email: req.body.email,
+        password: req.body.password,
+        read_only: false
+      }
+    ],
+    phone_numbers: req.body.phoneNumbers,
+    legal_names: req.body.legalNames,
+    extra: {
+      note: req.body.note,
+      supp_id: '',
+      is_business: req.body.isBusiness
+    }
+  }
+
+  Users.create(
+    client,
+    process.env.FINGERPRINT,
+    Helpers.getUserIP(),
+    payload,
+    (err, usersRes) => {
+      if (err) {
+        console.log(err);
+        return res.json(err);
+      }
+
+      return res.json(usersRes);
+    }
+  )
+})
+
+router.route('/:userID')
+.get((req, res) => {
+  const options = {
+    _id: req.params.userID,
+    fingerprint: '1234',
+    ip_address: Helpers.getUserIP(),
+    full_dehydrate: 'yes'
+  }
+
+  Users.get(
+    client,
+    options,
+    (err, usersRes) => {
+      if (err) {
+        return res.json(err);
+      }
+
+      return res.json(usersRes);
+    }
+  )
+})
+.post((req, res) => {
+  const options = {
+    _id: req.params.userID,
+    fingerprint: '1234',
+    ip_address: Helpers.getUserIP(),
+    full_dehydrate: 'yes'
+  }
+
+  const documentPayload = {
+    documents: [
+      req.body.documents
+    ]
+  }
+
+  Users.get(
+    client,
+    options
+    (err, user => {
+      if (err) {
+        return res.json(err);
+      }
+
+      user.addDocuments(
+        documentPayload,
+        (err, addDocRes => {
+          return res.json(addDocRes);
+        })
+      )
+    })
+  )
+})
+.put((req, res) => {
+  const options = {
+    _id: req.params.userID,
+    fingerprint: '1234',
+    ip_address: Helpers.getUserIP(),
+    full_dehydrate: 'yes'
+  }
+
+  const userUpdatePayload = {
+    documents: [
+      req.body.documents
+    ]
+  }
+
+  Users.get(
+    client,
+    options(err, user => {
+      if (err) {
+        return res.json(err);
+      }
+
+      user.update(
+        userUpdatePayload,
+        (err, updateDocRes => {
+          if (err) {
+            return res.json(err);
+          }
+          
+          return res.json(updateDocRes);
+        })
+      )
+    })
   )
 })
